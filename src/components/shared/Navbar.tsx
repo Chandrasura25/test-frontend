ec95263680f51335737cc83d38567c39d4d7f106
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-07-24 23:57:51 +0100

    changes

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,21 +1,37 @@
 import { motion } from 'framer-motion'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
-export default function Navbar(){
-	return(
+export default function Navbar() {
+	const location = useLocation();
+  
+	return (
 		<>
 			<motion.header
-		      initial="hidden"
-		      whileInView="show"
-		      className={`fixed top-0 w-full bg-[#9D9D9D] py-2 shadow-lg`}>
+				initial="hidden"
+				whileInView="show"
+				className="fixed top-0 w-full py-2 shadow-lg">
 				<div className='flex justify-between items-center px-8 py-3'>
 					<Link to='/' className='relative flex justify-center items-center gap-2'>
-						<p className='md:text-heading4-medium text-small-semibold text-white'>Test</p>
+						<p className='font-semibold text-lg uppercase text-white'>Test</p>
 					</Link>
 					<div >
 						<ul className='flex gap-4 text-white'>
-							<li><Link to='/'>Payment</Link></li>
-							<li><Link to='/transactions'>Transactions</Link></li>
+							<li>
+								<Link 
+									to='/' 
+									className={`${location.pathname === '/' ? 'text-[#fff38e]' : ''} hover:text-[#fff38e]`}
+								>
+									Payment
+								</Link>
+							</li>
+							<li>
+								<Link 
+									to='/transactions' 
+									className={`${location.pathname === '/transactions' ? 'text-[#fff38e]' : ''} hover:text-[#fff38e]`}
+								>
+									Transactions
+								</Link>
+							</li>
 						</ul>
 					</div>
 				</div>
