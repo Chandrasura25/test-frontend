1f30828c3cd2f60f9c7af4e043ccb36689d5746f
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-07-25 00:02:56 +0100

    improvement

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Transactions from '@/pages/Transactions'
 import Navbar from '@/components/shared/Navbar'
 function App() {
   return (
-    <div className="bg h-screen flex justify-center items-center">
+    <div className="bg flex justify-center items-center">
       <Navbar />
       <Routes>
         <Route path='/' element={<Payment />} />
