53af7005fb953818c8960d061854dff186f83b71
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-07-24 23:58:15 +0100

    changes

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -1,21 +1,24 @@
 import React, { useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import axios from 'axios';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import { axiosInstance } from '@/api/axios';
 import { toast } from 'react-toastify';
+import { CirclesWithBar } from 'react-loader-spinner';
 
 const Callback = () => {
   const [searchParams] = useSearchParams();
   const reference = searchParams.get('reference');
+  const navigate = useNavigate();
 
   useEffect(() => {
     const verifyTransaction = async () => {
       try {
-        const response = await axios.post('/api/verify-transaction', { reference });
+        const response = await axiosInstance.post('/verify-transaction', { reference });
         if (response.data.status === 'success') {
-          toast.success('Payment was successful');
-          // Perform any other necessary actions
+          toast.success(response.data.message);
+          navigate('/transactions');
         } else {
-          toast.error('Payment verification failed');
+          toast.error(response.data.message);
+          navigate('/');
         }
       } catch (err) {
         console.error(err);
@@ -26,10 +29,25 @@ const Callback = () => {
     if (reference) {
       verifyTransaction();
     }
-  }, [reference]);
+  }, [reference, navigate]);
 
-  return <div>Verifying Payment...</div>;
+  return (
+    <div className='flex flex-col justify-center items-center z-10 gap-4'>
+      <div>
+        <CirclesWithBar
+          height="100"
+          width="100"
+          color="#4fa94d"
+          outerCircleColor="#4fa94d"
+          innerCircleColor="#4fa94d"
+          barColor="#4fa94d"
+          ariaLabel="circles-with-bar-loading"
+          visible={true}
+        />
+      </div>
+      <p className='text-white font-semibold text-xl'>Verifying Payment...</p>
+    </div>
+  );
 };
 
 export default Callback;
-
